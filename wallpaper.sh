c9bcfe31cbb031b2337dbd204ee7622abc92944d
Richard Rowe <arch.richard@gmail.com>
2015-11-05 14:09:17 +1300

    collapse shell command into one line

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 mv /Library/Caches/com.apple.desktop.admin.png ~/Desktop/.backup
-mv
-$HOME/.dotfiles/lib/wallpapers/mountain_peak_stars_sky_night_light_snow_46057_2560x1440.jpg /Library/Caches/com.apple.desktop.admin.png
+mv $HOME/.dotfiles/lib/wallpapers/mountain_peak_stars_sky_night_light_snow_46057_2560x1440.jpg /Library/Caches/com.apple.desktop.admin.png
 
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "$HOME/.dotfiles/lib/wallpapers/mountain_peak_stars_sky_night_light_snow_46057_2560x1440.jpg"'
